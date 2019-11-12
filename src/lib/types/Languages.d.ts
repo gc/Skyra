79921d5a3fd056ce77ad7263fc8199bed9c969e1
gc <gc@users.noreply.github.com>
2019-11-12 12:37:59 +1100

    fix: price command doesnt show amount of the currency

--- src/lib/types/Languages.d.ts
+++ src/lib/types/Languages.d.ts
@@ -1047,7 +1047,7 @@ export interface LanguageKeys {
 	COMMAND_POLL_VOTE: string;
 	COMMAND_POLL_MISSING_ID: string;
 	COMMAND_POLL_EMPTY_VOTES: string;
-	COMMAND_PRICE_CURRENCY: (from: string, to: string, amount: number) => string;
+	COMMAND_PRICE_CURRENCY: (from: string, to: string, totalAmount: number, amount: number) => string;
 	COMMAND_PRICE_CURRENCY_NOT_FOUND: string;
 	COMMAND_QUOTE_MESSAGE: string;
 	COMMAND_ROLES_LIST_EMPTY: string;
